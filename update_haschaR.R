0d9c11b073b84b18ff97bb4ebac4e0ef8984f3a6
Sascha Riaz <riaz@g.harvard.edu>
2020-04-19 12:39:05 +0200

    added script to update package

---
+++ update_haschaR.R
@@ -0,0 +1,10 @@
+library(devtools)
+library(roxygen2)
+setwd('/Users/saschariaz/Google\ Drive_Harvard/Git/Rpackages/saschaR')
+
+document()
+
+## PUSH BEFORE REINSTALLING 
+
+install_github('https://github.com/saschariaz/saschaR', force = T)
+
