af09c41b93724aeccc53b5e5624c2d545446bda9
Sascha Riaz <riaz@g.harvard.edu>
2020-10-10 13:11:46 +0200

    added new function

---
+++ R/geometry_to_lon_lat.R
@@ -0,0 +1,21 @@
+#' A function to comnvert sf objects into data frames including lon/lat columns 
+#'
+#' 
+#' @param x an sf object
+#' @examples
+
+#' @export
+
+geometry_to_lonlat <- function(x) {
+  if (any(sf::st_geometry_type(x) != "POINT")) {
+    stop("Selecting non-points isn't implemented.")
+  }
+  coord_df <- sf::st_transform(x, sf::st_crs("+proj=longlat +datum=WGS84")) %>%
+    sf::st_coordinates() %>%
+    dplyr::as_tibble() %>%
+    dplyr::select(X, Y) %>%
+    dplyr::rename(lon = X, lat = Y)
+  out <- sf::st_set_geometry(x, NULL) %>%
+    dplyr::bind_cols(coord_df)
+  return(out)
+}
