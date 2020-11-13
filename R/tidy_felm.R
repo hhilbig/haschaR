b7ba672c2c329d05ba92c005fdc7985b4f1cf13a
Hanno <hannohilbig@gmail.com>
2020-11-13 11:27:46 +0100

    updates

--- R/tidy_felm.R
+++ R/tidy_felm.R
@@ -3,18 +3,37 @@
 #'
 #' @title Felm output summarize
 #' @param model felm model
+#' @param add_glance if T, glance output will be add to each line (note that not all default glance output will be returned)
 #' @import tidyverse lfe broom
 #' @export
 
 
-tidy_felm <- function(model) {
+tidy_felm <- function(model, add_glance = T) {
   
   n <- sum(!is.na(model$residuals))
   m_tidy <- broom::tidy(model, conf.int = T)
   
   ## Combine and return
   
-  m_tidy %>% 
+  out <- m_tidy %>% 
     mutate(n = n)
+  
+  if (add_glance) {
+    g <- model %>% glance() %>% 
+      dplyr::rename(f_stat = statistic,
+                    f_pval = p.value) %>% 
+      dplyr::select(matches('squared|f_'))
+    
+    out <- out %>% 
+      mutate(rsq = g$r.squared,
+             a_rsq = g$adj.r.squared,
+             f_stat = g$f_stat,
+             f_pval = g$f_pval)
+    
+    ## return
+    
+    out
+  } else {
+    out
+    }
 }
-
