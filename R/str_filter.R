74e8902b6fbf0b9d065d0106f9c0e30dcdd2f697
Hanno <hannohilbig@gmail.com>
2020-12-13 19:13:00 +0100

    updates

--- R/str_filter.R
+++ R/str_filter.R
@@ -2,12 +2,13 @@
 #'
 #' @param string list of felm models to be passed to stargazer 
 #' @param pattern name of explanatory variable to be replaced
+#' @param neg if T, negate
 #' @return Returns string
 #' @import tidyverse stringr
 #' @export
 
-str_filter <- function(string, pattern) {
+str_filter <- function(string, pattern, neg = F) {
   
-  string %>% .[str_detect(., pattern)]
+  string %>% .[str_detect(., pattern), negate = neg]
   
 }
