63b663e0fd8cc2c536c894e3077fd775f4d0c944
Hanno <hannohilbig@gmail.com>
2021-02-24 13:57:04 +0100

    update read all excel sheets

--- R/read_excel_allsheets.R
+++ R/read_excel_allsheets.R
@@ -3,12 +3,13 @@
 #' @title Read all worksheets 
 #' @param filename
 #' @param tibble
+#' @param ... arguments passed to read_excel
 #' @export
 #' @examples
 
-read_excel_allsheets <- function(filename, tibble = FALSE) {
+read_excel_allsheets <- function(filename, tibble = FALSE, ...) {
   sheets <- readxl::excel_sheets(filename)
-  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X, ...))
   if(!tibble) x <- lapply(x, as.data.frame)
   names(x) <- sheets
   x
