eb75bde38a46c5009de9c4b1151d0694f05affa0
Sascha Riaz <riaz@g.harvard.edu>
2021-03-12 11:37:48 +0100

    added median to summarize vec

--- R/summarize_vector.R
+++ R/summarize_vector.R
@@ -9,12 +9,14 @@
 summarize_vec <- function(vector){
   
   mean_v <- mean(vector, na.rm = T)
+  median_v <- median(vector, na.rm = T)
   sd_v <- sd(vector, na.rm = T)
   n_obs <- length(vector[!is.na(vector)])
   min_v <- min(vector, na.rm = T)
   max_v <- max(vector, na.rm = T)
   
   
-  data.frame(cbind(mean_v, sd_v, n_obs, min_v, max_v))
+  data.frame(cbind(mean_v, median_v, sd_v, n_obs, min_v, max_v))
   
 }
+
