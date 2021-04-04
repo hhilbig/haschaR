e096abc6e28750c05100f4a136c91a4b75f21daa
Hanno <hannohilbig@gmail.com>
2021-04-04 17:47:22 +0200

    updates

---
+++ R/drop_missings_df.R
@@ -0,0 +1,11 @@
+#' Returns DF without missings
+#'
+#' @param df df
+#' @param varlist list of vars to be considered for dropping missings
+#' @return DF with original string, match and distance to matchs
+#' @export
+
+fdrop_missings_df <- function(df, varlist) {
+  df <- df[complete.cases(df[, varlist])]
+  df
+}
