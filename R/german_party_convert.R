59d35f644d9316a1ffb484ded445b9d51759fd60
Hanno <hannohilbig@gmail.com>
2022-05-11 13:27:35 -0400

    add german party convert

---
+++ R/german_party_convert.R
@@ -0,0 +1,40 @@
+#' Convert german party names (de or en) to proper German party names
+#'
+#' @param party_vec vector of party names
+#' @import tidyverse stringr
+#' @export
+#' 
+
+german_party_convert <- function(party_vec) {
+  
+  ## Party origins and destinations
+  
+  party_orig <- list(c('spd', 'SPD'),
+                     c('CDU', 'cdu', 'csu', 'c.s.u', 'christ'),
+                     c('LINKE', 'linkspartei', 'pds', 'links',
+                       'left'),
+                     c('greens', 'Gruene', 'green', 'gruen'),
+                     c('fdp', 'f.d.p.'),
+                     c('AFD', 'afd', 'a.f.d.', 'alternative'))
+  party_destination <- c('SPD', 'CDU/CSU', 'Left', 'Greens', 'FDP', 'AfD')
+  
+  ## Loop
+  
+  party_vec %>% sapply(function(x) {
+    
+    which_party <- sapply(party_orig, function(p) {
+      
+      str_detect(x, paste0(p, collapse = '|'))
+      
+    })
+    
+    ## If nothing found, return the original value
+    
+    if (all(!which_party)) {
+      x
+    } else {
+      party_destination[which_party]
+    }
+    
+  }) %>% unname()                   
+}
