9b61f22dd0fba89439f555bf3e09bcf566cf3fa4
Hanno <hannohilbig@gmail.com>
2023-01-12 18:31:11 +0100

    updated tidy_feols

--- R/tidy_feols.R
+++ R/tidy_feols.R
@@ -1,72 +1,22 @@
-#' feols output summarize
+#' feols output summarize for multiple outcomes (should also work for just one outcome)
 #'
-#' @title feols output summarize
-#' @param model feols model
-#' @param add_glance if T, glance output will be added to each line (note that not all default glance output will be returned)
-#' @param add_dv_stats if T, DV mean, sd, min, max will be added to each line
-#' @param add_conf_90 if T, 90\% CIs will be added (note that I use normal and not t distribution)
+#' @title feols output summarize for multiple outcomes
+#' @param model feols model with multiple outcomes
+#' @param ... arguments passed to tidy_feols_single (see documentation)
 #' @import tidyverse fixest broom
 #' @export
 
 
-tidy_feols <- function(model, add_glance = T,
-                      add_dv_stats = T,
-                      add_conf_90 = T) {
+tidy_feols <- function(model, ...) {
   
-  n <- sum(!is.na(model$residuals))
-  m_tidy <- broom::tidy(model, conf.int = T)
-  dv <- model.matrix(model, type = 'lhs')
-  
-  ## Combine and return
-  
-  out <- m_tidy %>% 
-    mutate(n = n)
-  
-  ## 90% CI
-  
-  if (add_conf_90) {
+  if (names(model)[1] == 'nobs') {
     
-   out <- out %>% 
-     mutate(conf.low90 = estimate - qnorm(0.95) * std.error,
-            conf.high90 = estimate + qnorm(0.95) * std.error)
+    tidy_feols_single(model, ...)
     
-  }
-  
-  ## Add Mean, SD, Min, Max of DV
-  
-  dv_mean <- dv %>% mean(na.rm = T)
-  dv_sd <- dv %>% sd(na.rm = T)
-  dv_min <- dv %>% min(na.rm = T)
-  dv_max <- dv %>% max(na.rm = T)
-  
-  ## Add DV stats to output
-  
-  if (add_dv_stats) {
-    
-    out <- out %>% 
-      mutate(dv_mean = dv_mean,
-             dv_sd = dv_sd,
-             dv_min = dv_min,
-             dv_max = dv_max)
-    
-  }
-  
-
-  
-  ## Add model stats
-  
-  if (add_glance) {
-    g <- model %>% broom::glance() %>% 
-      dplyr::select(matches('squared|f_'))
-    
-    out <- out %>% 
-      mutate(rsq = g$r.squared,
-             a_rsq = g$adj.r.squared)
-    
-    ## return
-    
-    out
   } else {
-    out
-    }
+    
+    lapply(model, function(x) {
+      tidy_feols_single(x, ...)
+    }) %>% reduce(bind_rows)
+  }
 }
---
+++ R/tidy_feols_single.R
@@ -0,0 +1,93 @@
+#' feols output summarize
+#'
+#' @title feols output summarize
+#' @param model feols model
+#' @param add_glance if T, glance output will be added to each line (note that not all default glance output will be returned)
+#' @param add_dv_stats if T, DV mean, sd, min, max will be added to each line
+#' @param add_conf_90 if T, 90\% CIs will be added (note that I use normal and not t distribution)
+#' @import tidyverse fixest broom
+#' @export
+
+
+tidy_feols_single <- function(model, add_glance = T,
+                              add_dv_stats = T,
+                              add_conf_90 = T) {
+  
+  ## Check if multiple outcome
+  
+  if (!names(model)[1] == 'nobs') {
+    
+    cat('Looks like this model uses more than one outcome;\nPlease use tidy_feols instead')
+    
+  } else {
+    
+    n <- model$nobs
+    m_tidy <- broom::tidy(model, conf.int = T)
+    dv_val <- model.matrix(model, type = 'lhs')
+    
+    ## Combine and return
+    
+    out <- m_tidy %>% 
+      mutate(n = n)
+    
+    ## 90% CI
+    
+    if (add_conf_90) {
+      
+      out <- out %>% 
+        mutate(conf.low90 = estimate - qnorm(0.95) * std.error,
+               conf.high90 = estimate + qnorm(0.95) * std.error)
+      
+    }
+    
+    ## Add Mean, SD, Min, Max of DV
+    
+    dv_mean <- dv_val %>% mean(na.rm = T)
+    dv_sd <- dv_val %>% sd(na.rm = T)
+    dv_min <- dv_val %>% min(na.rm = T)
+    dv_max <- dv_val %>% max(na.rm = T)
+    
+    ## Add DV stats to output
+    
+    if (add_dv_stats) {
+      
+      out <- out %>% 
+        mutate(dv_mean = dv_mean,
+               dv_sd = dv_sd,
+               dv_min = dv_min,
+               dv_max = dv_max)
+      
+    }
+    
+    ## Add model formula and variables
+    
+    fml <- model$fml
+    dv_lab <- fml %>% as.character() %>% 
+      {.[2]}
+    fml <- Reduce(paste, deparse(fml)) %>% str_squish() %>% 
+      str_replace_all('"', '')
+    
+    out <- out %>% 
+      mutate(dv = dv_lab, 
+             fml = deparse(fml))
+    
+    ## Add model stats
+    
+    if (add_glance) {
+      g <- model %>% broom::glance() %>% 
+        dplyr::select(matches('squared|f_'))
+      
+      out <- out %>% 
+        mutate(rsq = g$r.squared,
+               a_rsq = g$adj.r.squared)
+      
+      ## return
+      
+      out %>% 
+        dplyr::select(term, dv, fml, everything())
+    } else {
+      out %>% 
+        dplyr::select(term, dv, fml, everything())
+    }
+  }
+}
