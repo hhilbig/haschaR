b8d7e4b35fd2876792247e4bf9c320c8f8b04c84
Hanno <hannohilbig@gmail.com>
2023-04-30 12:31:05 -0400

    updates

--- update_haschaR.R
+++ update_haschaR.R
@@ -5,14 +5,13 @@ rm(list = ls())
 
 ## Declare WDs
 
-wd_h <- '/Users/hanno/Local_Projects/haschaR/'
-wd_h2 <- '/Users/hhilbig/Documents/GitHub/haschaR/'
+wd_h <- '~/Documents/GitHub/haschaR/'
 wd_s <- '/Users/saschariaz_1/Dropbox\ (Nuffield\ College)/07_Git/haschaR'
 
 ## Set WD
 
-haschaR::detect_wd(wd_h = wd_h, wd_alt = wd_s, user_name = 'Hanno',
-                   wd_h2 = wd_h2, user_name2 = "hhilbig")
+haschaR::detect_wd(wd_h = wd_h, wd_alt = wd_s, user_name = 'hanno',
+                   wd_h2 = wd_h2)
 
 document()
 
